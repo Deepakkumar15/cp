f52494b30a82ed12d934a725aa9d72de624b9000
Deepak Kumar <55249998+Deepakkumar15@users.noreply.github.com>
2021-02-27 11:49:01 +0530

    Add files via upload

--- DSA/DP/coin_change.cpp
+++ DSA/DP/coin_change.cpp
@@ -1,3 +1,5 @@
+// COIN CHANGE PROBLEM
+// RECURSION + MEMOIZATION SOLUTION
 class Solution{
     public:
         long long int dp[1005][1005] ;
@@ -20,4 +22,32 @@ class Solution{
                 return dp[m][n] = count(S, m-1, n) ;
             
         }
+};
+
+
+
+// ITERATIVE DP VERSION
+class Solution{
+    public:
+                
+        long long int count( int S[], int m, int n ){
+            long long int dp[m+1][n+1] ;
+            for(int i=0; i<=m; i++)
+                dp[i][0]=1 ;
+            for(int j=1; j<=n; j++)
+                dp[0][j]=0 ;
+                
+            for(int i=1; i<=m; i++){
+                for(int j=1; j<=n; j++){
+                    if(S[i-1]<=j)
+                        dp[i][j] = dp[i-1][j] + dp[i][j-S[i-1]] ;
+                    else
+                        dp[i][j] = dp[i-1][j] ;
+                }
+            }    
+            
+            return dp[m][n] ;
+            
+        }
+
 };
