5a162c38995d8e85ce3b09b13acf5cd84f570574
Deepak Kumar <55249998+Deepakkumar15@users.noreply.github.com>
2021-07-15 15:01:39 +0530

    Add files via upload

---
+++ DSA/STACK/next_smaller_element.cpp
@@ -0,0 +1,48 @@
+// NEXT SMALLER ELEMENT USING STACK
+
+#define ll long long int
+vector<ll> nextSmallerElement(vector<ll> a, ll n){
+    vector<ll> nge(n, -1) ;
+    stack<ll> st ;
+    
+    // process from back
+    for(ll i=n-1; i>=0; i--){
+        // pop condition
+        while(!st.empty() && st.top() >= a[i])
+            st.pop() ;
+            
+        if(!st.empty())
+            nge[i] = st.top() ;
+            
+        // else there will be always -1
+        st.push(a[i]) ;
+    }
+    
+    return nge ;
+}
+
+
+
+
+
+// NEXT SMALLER ELEMENT IN CIRCULAR FASHION (SLIGHT MODIFICATION)
+#define ll long long int
+vector<ll> nextSmallerElement(vector<ll> a, ll n){
+    vector<ll> nge(n, -1) ;
+    stack<ll> st ;
+    
+    // process from back
+    for(ll i=2*n-1; i>=0; i--){
+        // pop condition
+        while(!st.empty() && st.top() >= a[i%n])
+            st.pop() ;
+            
+        if(!st.empty() && i<n)
+            nge[i] = st.top() ;
+            
+        // else there will be always -1
+        st.push(a[i]%n) ;
+    }
+    
+    return nge ;
+}
